080892874d9ffb09fd8f770ddf03f7d09c36d7b7
Jackeagle <jackeagle102@gmail.com>
2020-06-08 19:22:04 +0200

    sm8250-common: Build libvndfwk_detect_jni.qti.vendor
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: I45a11169c0382db0305ac0639903df03385ec38b

--- kona.mk
+++ kona.mk
@@ -339,6 +339,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti.vendor # Needed by CNE app
 
+# QTI fwk-detect
+PRODUCT_PACKAGES += \
+    libvndfwk_detect_jni.qti.vendor # Needed by CNE app
+
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.qcom \
