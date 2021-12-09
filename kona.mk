dfc08fe8962bdf7fb8a34969bc39e81f4f2c8333
Zinadin Zidan <zidan.roking@gmail.com>
2021-12-09 04:57:19 +0200

    sm8250-common: Guard qti thermal 2.0 service hals
    * Crashing on Alioth and some other devices
    
    * Crash:
        - E android.hardware.thermal@2.0-service.qti: ThermalHAL not initialized properly.
        - E ThermalHalWrapper: Couldn't get temperatures because of HAL error: ThermalHAL not initialized properly.

--- kona.mk
+++ kona.mk
@@ -387,8 +387,10 @@ PRODUCT_BOOT_JARS += \
     xiaomi-telephony-stub
 
 # Thermal
+ifeq ($(TARGET_USE_QTI_THERMAL_SERVICE),true)
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.qti
+endif
 
 # USB
 PRODUCT_HAS_GADGET_HAL := true
