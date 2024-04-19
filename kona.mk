129e37bd23f49d207692a01789bf1a9bd383134e
Quick ⚡ <quick@disroot.org>
2024-04-19 07:08:43 +0530

    sm8250-common: Use Cloudflare as primary NTP server
    * Cloudflare is LOVE :)
    * https://blog.cloudflare.com/secure-time
    * manage gps.conf from tree
    * Set 2,3 servers to windows & apple ones

--- kona.mk
+++ kona.mk
@@ -319,7 +319,8 @@ PRODUCT_PACKAGES += \
     libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
+    $(LOCAL_PATH)/configs/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf
 
 # Public libraries
 PRODUCT_COPY_FILES += \
