d14392889c7216fda157b4bb2662d8885cb2307e
EmanuelCN <emanuelghub@gmail.com>
2024-09-26 00:15:44 +0530

    sm8250-common: Rename libperfmgr to xiaomi-sm8250-libperfmgr

--- kona.mk
+++ kona.mk
@@ -431,7 +431,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr \
+    android.hardware.power-service.xiaomi-sm8250-libperfmgr \
     libqti-perfd-client \
     android.hardware.power@1.2.vendor
 
