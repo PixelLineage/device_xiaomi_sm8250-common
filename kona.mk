52cb3ce7c0458d8886402442d583484f12adf016
Michael Bestas <mkbestas@lineageos.org>
2025-11-28 13:13:53 +0200

    sm8250-common: overlay: Remove Dialer overlay
    Call recording is now enabled by default
    
    Change-Id: Ie454647046b9a2e4b2129e3f04352dd08073c744

--- kona.mk
+++ kona.mk
@@ -331,7 +331,6 @@ TARGET_SUPPORTS_OMX_SERVICE := false
 PRODUCT_PACKAGES += \
     CarrierConfigOverlayCommon \
     FrameworkResOverlayCommon \
-    LineageDialerOverlayCommon \
     LineageSDKOverlayCommon \
     LineageSettingsOverlayCommon \
     SettingsOverlayCommon \
